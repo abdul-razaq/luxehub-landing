0935e2d676602bbefbe00fd269cc3051c99ddf50
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2024-07-15 17:18:01 +0100

    fix: fixed shopping section images

--- src/components/ShoppingSection.tsx
+++ src/components/ShoppingSection.tsx
@@ -4,7 +4,7 @@ import { FaArrowRightLong } from 'react-icons/fa6';
 export default function ShoppingSection() {
 	return (
 		<article>
-			<section className="pb-20">
+			<section className="pb-20 px-24 py-28">
 				<div className="flex justify-between mb-6 px-4">
 					<h2 className="font-SpaceGrotesk uppercase tracking-tight text-lg md:text-xl leading-normal">
 						Shop Men's Wear
@@ -16,16 +16,16 @@ export default function ShoppingSection() {
 				</div>
 
 				<div className="flex flex-col md:flex-row justify-center items-center gap-4 max-w-full flex-wrap">
-					<img src="/public/assets/images/shopping/men/men-1.png" alt="men-1" />
-					<img src="/public/assets/images/shopping/men/men-2.png" alt="men-2" />
-					<img src="/public/assets/images/shopping/men/men-3.png" alt="men-3" />
-					<img src="/public/assets/images/shopping/men/men-4.png" alt="men-4" />
-					<img src="/public/assets/images/shopping/men/men-5.png" alt="men-5" />
-					<img src="/public/assets/images/shopping/men/men-6.png" alt="men-6" />
+					<img src="/assets/images/shopping/men/men-1.png" alt="men-1" />
+					<img src="/assets/images/shopping/men/men-2.png" alt="men-2" />
+					<img src="/assets/images/shopping/men/men-3.png" alt="men-3" />
+					<img src="/assets/images/shopping/men/men-4.png" alt="men-4" />
+					<img src="/assets/images/shopping/men/men-5.png" alt="men-5" />
+					<img src="/assets/images/shopping/men/men-6.png" alt="men-6" />
 				</div>
 			</section>
 
-			<section>
+			<section className="px-24 py-28">
 				<div className="flex justify-between mb-6 px-4">
 					<h2 className="font-SpaceGrotesk uppercase tracking-tight text-lg md:text-xl leading-normal">
 						Shop Women's Wear
@@ -37,30 +37,12 @@ export default function ShoppingSection() {
 				</div>
 
 				<div className="flex flex-col md:flex-row justify-center items-center gap-4 max-w-full flex-wrap">
-					<img
-						src="/public/assets/images/shopping/women/women-1.png"
-						alt="women-1"
-					/>
-					<img
-						src="/public/assets/images/shopping/women/women-2.png"
-						alt="women-2"
-					/>
-					<img
-						src="/public/assets/images/shopping/women/women-3.png"
-						alt="women-3"
-					/>
-					<img
-						src="/public/assets/images/shopping/women/women-4.png"
-						alt="women-4"
-					/>
-					<img
-						src="/public/assets/images/shopping/women/women-5.png"
-						alt="women-5"
-					/>
-					<img
-						src="/public/assets/images/shopping/women/women-6.png"
-						alt="women-6"
-					/>
+					<img src="/assets/images/shopping/women/women-1.png" alt="women-1" />
+					<img src="/assets/images/shopping/women/women-2.png" alt="women-2" />
+					<img src="/assets/images/shopping/women/women-3.png" alt="women-3" />
+					<img src="/assets/images/shopping/women/women-4.png" alt="women-4" />
+					<img src="/assets/images/shopping/women/women-5.png" alt="women-5" />
+					<img src="/assets/images/shopping/women/women-6.png" alt="women-6" />
 				</div>
 			</section>
 		</article>
